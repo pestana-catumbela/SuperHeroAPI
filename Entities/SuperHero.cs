8c3f85a044c94b7c1380a0b0c44e44d745e6b6ce
Pestana Pedro Catumbela <pestana.catumbela@beesby.com>
2024-03-30 16:33:00 -0300

    Feat: add entities

---
+++ Entities/SuperHero.cs
@@ -0,0 +1,11 @@
+﻿namespace SuperHeroAPI.Entities
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+    }
+}
